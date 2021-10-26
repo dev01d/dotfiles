205b4e582badbd32aa7a2197c815576282684009
Jason Behnke <contact@jasonbehnke.io>
2021-10-26 01:48:25 -0700

    Brew maintenance script

---
+++ brew_maint.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import subprocess
+
+
+def runBrewMaint():
+    print("\n--> Brew install from Brewfile", flush=True)
+    subprocess.run("brew bundle --cleanup --no-lock", shell=True, check=True)
+
+    print("\n--> Brew upgrade casks", flush=True)
+    subprocess.run("brew cu -facy --quiet", shell=True, check=True)
+
+    print("\n--> Update Brewfile", flush=True)
+    subprocess.run("brew bundle dump -f", shell=True, check=True)
+
+
+def main():
+    runBrewMaint()
+
+
+if __name__ == "__main__":
+    main()
