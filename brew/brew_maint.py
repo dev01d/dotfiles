00cad164dbdbdf779783b3b76b27cb09bc14f6c4
Jason Behnke <jason@jasonbehnke.io>
2022-01-23 16:04:51 -0800

    Move

--- brew/brew_maint.py
+++ brew/brew_maint.py
@@ -3,14 +3,15 @@ import subprocess
 
 
 def runBrewMaint():
-    print("\n--> Brew install from Brewfile", flush=True)
-    subprocess.run("brew bundle --cleanup --no-lock", shell=True, check=True)
-
     print("\n--> Brew upgrade casks", flush=True)
     subprocess.run("brew cu -facy --cleanup", shell=True, check=True)
 
     print("\n--> Update Brewfile", flush=True)
-    subprocess.run("brew bundle dump -f", shell=True, check=True)
+    subprocess.run("brew bundle dump --cleanup -f", shell=True, check=True)
+
+    print("\n--> Brew install from Brewfile", flush=True)
+    subprocess.run("brew bundle --cleanup --no-lock",
+                   shell=True, check=True)
 
 
 def main():
