cd9e2b6235dfab32e608acda92e593baa883dc09
Jason Behnke <jason@jasonbehnke.io>
2022-03-26 19:19:02 -0700

    Update script

--- brew/brew_maint.py
+++ brew/brew_maint.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
+
 import subprocess
 
 
+def blue(text):
+    return "\033[34m{} \033[0m".format(text)
+
+
+def green(text):
+    return "\033[32m{} \033[0m".format(text)
+
+
 def runBrewMaint():
-    print("\n--> Brew upgrade casks", flush=True)
+    print(blue("\n--> Brew upgrade casks\n"))
     subprocess.run("brew cu -facy --cleanup", shell=True, check=True)
 
-    print("\n--> Update Brewfile", flush=True)
+    print(blue("\n--> Update Brewfile\n"))
     subprocess.run("brew bundle dump --cleanup -f", shell=True, check=True)
 
-    print("\n--> Brew install from Brewfile", flush=True)
+    print(blue("\n--> Brew install from Brewfile\n"))
     subprocess.run("brew bundle --cleanup --no-lock",
                    shell=True, check=True)
 
 
 def main():
     runBrewMaint()
+    print(green("\n✨ Done!\n"))
 
 
 if __name__ == "__main__":
